16b94a93c9e6a7c5a7e74ae66b8a3dd1c8b675ff
Jibran Khan <Jibran.khan@iii.co.uk>
2014-10-28 12:18:44 +0000

    updates

--- server/server.php
+++ server/server.php
@@ -1,6 +1,6 @@
 <?php
  
-  //generate verification code :
+  
 
   // $string = substr(str_shuffle(abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ),0, 1) . substr(str_shuffle(aBcEeFgHiJkLmNoPqRstUvWxYz0123456789),0, 31);
   // $code = substr($string, 0, 6);
@@ -9,7 +9,8 @@
   
 
   $type = $_POST['type'];
-
+ 
+ //function call
   $type();
 
   function signin(){
@@ -20,12 +21,18 @@
 
 
   function register(){
-    
+     
+     //postdata
      $name = $_POST['name'];
      $email = hash('sha256', $_POST['email']);
      $phone = $_POST['phone'];
      $password = hash('sha256', $_POST['password']);
      $confirm = hash('sha256', $_POST['confirm']);
+     
+     //generate verification code :
+	 $string = substr(str_shuffle(abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ),0, 1) . substr(str_shuffle(aBcEeFgHiJkLmNoPqRstUvWxYz0123456789),0, 31);
+	 $ver_code = substr($string, 0, 6);
+		
    
 
         
