1221655d9aab378e2a033d4fe195980b1069477d
EC2 Default User <ec2-user@ip-172-31-26-46.us-west-2.compute.internal>
2014-10-29 12:08:21 +0000

    phonegap install

--- manifest.php
+++ manifest.php
@@ -1,27 +1,12 @@
-<<<<<<< HEAD
 <?php
 header('Content-Type: text/cache-manifest');
 $filesToCache = array(
-    './index.html', 
-    './js/todo7.js', 
-    './css/todo7.css', 
-    './img/bg.jpg', 
-    '../../dist/js/framework7.min.js', 
-    '../../dist/css/framework7.min.css'
-=======
-
-<?php
-header('Content-Type: text/cache-manifest');
-$filesToCache = array(
-
     // './index.html', 
     // './js/todo7.js', 
     // './css/todo7.css', 
     // './img/bg.jpg', 
     // '../../dist/js/framework7.min.js', 
     // '../../dist/css/framework7.min.css'
-
->>>>>>> a153e997866ab212f760a1ff680a1959e3ab9e68
 );
 ?>
 CACHE MANIFEST
@@ -39,10 +24,4 @@ foreach($filesToCache as $file) {
 NETWORK:
 *
 
-<<<<<<< HEAD
-# Hash Version: <?=md5($hashes)?>
-=======
-# Hash Version: <?=md5($hashes)
-	
-?>
->>>>>>> a153e997866ab212f760a1ff680a1959e3ab9e68
+# Hash Version: <?=md5($hashes)?
